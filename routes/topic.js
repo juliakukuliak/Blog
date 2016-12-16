b1379ad7dd42e21af2d586c3543d6d642ac7729a
juliakukuliak <juliakukuliak@gmail.com>
2016-12-16 09:26:14 +0200

    initial

---
+++ routes/topic.js
@@ -0,0 +1,57 @@
+var express = require('express');
+var Topic = require('./../model/topicModel');
+var router = express.Router();
+var fs = require('fs');
+
+var topics = initTopics();
+
+router
+    .get('/', function (req, res, next) {
+        res.json(topics.map(function (el) {
+            return el.getSimpleModel();
+        }));
+    })
+    .get('/:name/', function (req, res, next) {
+        var topic = topics.find(function (elem) {
+            return elem.name == req.params.name;
+        });
+        if(topic) {
+            res.json(topic);
+        } else {
+            var err = new Error('Not Found');
+            err.status = 404;
+            next(err);
+        }
+    })
+    .post('/', function (req, res, next) {
+        var body = req.body;
+        console.log("text", body);
+        topics.push(new Topic(body.name, body.author, body.text, body.date));
+        res.send(200);
+    })
+    .put('/:name/', function (req, res, next) {
+        var body = req.body;
+        topics = topics.filter(function (el) {
+            return el.name !== req.params.name
+        });
+        topics.push(new Topic(body.name, body.author, body.text, body.date));
+    })
+    .delete('/:name', function (req, res, next) {
+        var name = req.params.name;
+        if(name == undefined || name.length == 0) throw new Error("Name should be request parameter");
+        topics = topics.filter(function (el) {
+            return el.name !== name
+        });
+        res.send(200);
+    });
+
+function initTopics() {
+    var topics = [];
+    var topicsData = JSON.parse(fs.readFileSync('./data/topics.json'));
+    topicsData.topics.forEach(function (element) {
+        topics.push(new Topic(element.name, element.author, element.text, element.data));
+    });
+    return topics;
+}
+
+module.exports = router;
